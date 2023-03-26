5ff68ac12e81e604d4b236e473ebc841ab3ee52b
Nher10 <nherwinostia2@gmail.com>
2023-03-27 00:19:27 +0800

    finished product API

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+//create product
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get all products
+router.get("/get-product", getProductController);
+
+//get single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/product/:pid", deleteProductController);
+
+export default router;
